94c3ccffcea92ea6db099c54f8a9a7d4c53a9c7f
StefanMS <stefan_statescu@yahoo.com>
2024-12-02 14:58:42 +0200

    user_collection design

--- nuxt-app/components/NavBar.vue
+++ nuxt-app/components/NavBar.vue
@@ -1,35 +1,108 @@
 <template>
   <div class="navbar">
-    <header class="navbar-header">EasyGames</header>
     <nav class="navbar-links">
-      <button class="navbar-button" @click="navigateHome">Home</button>
-      <button v-if="userLoggedIn" class="navbar-button" @click="navigateAccount">Account</button>
+      <span v-if="user" class="navbar-welcome">Welcome, {{ user.first_name }}!</span>
+      <div>EasyGames</div>
+      <button class="navbar-button" @click="navigateHome">HOME</button>
+
       <button v-if="!userLoggedIn" class="navbar-button" @click="handleSignup">Sign Up</button>
       <button v-if="!userLoggedIn" class="navbar-button" @click="handleLogin">Login</button>
-      <button v-else class="navbar-button" @click="handleLogout">Logout</button>
+
+      <div v-if="userLoggedIn" class="navbar-dropdown">
+        <div class="navbar-account">
+          <div class="navbar-account-icon"></div>
+        </div>
+
+        <div class="dropdown-content">
+          <button class="dropdown-item" @click="navigateAccount">Account</button>
+          <button class="dropdown-item" @click="handleLogout">Logout</button>
+        </div>
+      </div>
+
     </nav>
+  </div>
+
+    <!-- TODO: different component -->
+  <div class="game-banner">
+    <div class="game-banner-header">
+      GAME OF THE WEEK
+    </div>
+    <div class="game-banner-image">
+      MINECRAFT
+    </div>
+  </div>
+
+
+
+  <!-- TODO: different component -->
+  <div v-if="user"class="wallet-banner">
+    <div class="wallet-container">
+      <div class="wallet-label">WALLET</div>
+        <div class="wallet-value">
+          {{ user.balance }}
+          <span class="coin-emoji">🪙</span>
+        </div>
+    </div>
   </div>
 </template>
 
-<script setup>
-import { ref, onMounted, onBeforeUnmount } from 'vue';
+
+<script setup lang="ts">
+import { ref, onMounted, onBeforeUnmount, nextTick } from 'vue';
 import { useRouter } from 'vue-router';
-import "@/assets/css/navbar-header.css";
+import "@/assets/css/navbar.css";
+import { useCurrentUser } from '~/composables/useJwt';
+import type { User } from "~/types/user";
 
 const userLoggedIn = ref(false);
 const router = useRouter();
+const user = ref<User | null>(null);
+const config = useRuntimeConfig()
 
-const checkLoginStatus = () => {
-  const token = localStorage.getItem('access_token');
-  userLoggedIn.value = !!token;
+const checkLoginStatus = async () => {
+  const accessToken = localStorage.getItem('access_token');
+  userLoggedIn.value = !!accessToken;
+  const currentUser = useCurrentUser(accessToken);
+
+  if (currentUser) {
+    try {
+      const userId = currentUser.userId;
+      const response = await fetch(`${config.public.apiUrl}/users/${userId}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Failed to fetch user details: ${response.status} ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      user.value = data as User;
+
+      await nextTick(); // Ensures updates are reflected in the DOM
+    } catch (error) {
+      console.error('An error occurred while fetching the user details:', error);
+    }
+  } else {
+    console.error('No current user found');
+  }
 };
+
+
+
+
+
+
 
 const updateLoginStatus = () => {
   userLoggedIn.value = !!localStorage.getItem('access_token');
+
 };
 
 onMounted(() => {
   checkLoginStatus();
+  
 
   window.addEventListener('storage', updateLoginStatus);
 });
@@ -45,6 +118,7 @@ const handleLogin = () => {
 const handleLogout = () => {
   localStorage.removeItem('access_token');
   userLoggedIn.value = false;
+  user.value = null;
   router.push('/collection');
 };
 
