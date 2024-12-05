d890a5465f7604961317bc4563c24e433dbf07d5
StefanMS <stefan_statescu@yahoo.com>
2024-12-05 17:25:10 +0200

    index page

---
+++ nuxt-app/components/IndexPage.vue
@@ -0,0 +1,25 @@
+<template>
+    <div v-if="accessToken">
+        <UserFilteredColelction />
+    </div>
+    <div v-else>
+        <Collection />
+    </div>
+</template>
+
+<script lang="ts">
+
+import { onMounted } from 'vue';
+
+onMounted(() => {
+    checkToken();
+})
+
+
+const checkToken = () => {
+    const accessToken = localStorage.getItem('access_token');
+    return !!accessToken;
+}
+
+
+</script>
--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -1,5 +1,3 @@
 <template>
-    <div>
-        <h1>Index</h1>
-    </div>
+    <IndexPage />
 </template>
